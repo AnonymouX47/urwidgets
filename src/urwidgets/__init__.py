f81625adc17aac99726902dfbbd346a228359e57
AnonymouX47 <anonymoux47@gmail.com>
2023-04-22 14:19:19 +0100

    chore: Add package info and metadata

--- src/urwidgets/__init__.py
+++ src/urwidgets/__init__.py
@@ -1,1 +1,17 @@
-"""A collection of widgets for urwid (https://urwid.org)"""
+"""
+urWIDgets
+
+A collection of widgets for urwid (https://urwid.org)
+
+Copyright (c) 2023, Toluwaleke Ogundipe <anonymoux47@gmail.com>
+"""
+
+__all__ = ()
+__author__ = "Toluwaleke Ogundipe"
+
+version_info = (0, 1, 0, "dev")
+
+# Follows https://semver.org/spec/v2.0.0.html
+__version__ = ".".join(map(str, version_info[:3]))
+if version_info[3:]:
+    __version__ += "-" + ".".join(map(str, version_info[3:]))
