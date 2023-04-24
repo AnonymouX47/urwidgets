4e9e604b519649ab07fc9ba3a7b7b46d32a47f8c
AnonymouX47 <anonymoux47@gmail.com>
2023-04-24 14:38:08 +0100

    refactor: Rename variables and cleanup code
    - Change: Rename some variables and class attributes for better clarity.
    - Change: Remove unnecessary code.
    - Change: Correct and update comments.

--- src/urwidgets/text_embed.py
+++ src/urwidgets/text_embed.py
@@ -8,21 +8,20 @@ import urwid
 
 
 class TextEmbed(urwid.Text):
-    # Used to guard the execution of `._update_canv_start_pos()`
+    # Used to prevent multiple executions of `._update_canv_start_pos()` while
+    # initializing or setting layout
     _initialized = True
-    _layout_is_set = True
+    _setting_layout = False
 
-    # In case a placeholder gets wrapped:
-    # - will match only the starting portion of a placeholder
-    # - not trailing portions on subsequent lines
-    _placeholder = re.compile("(\0\1*)")
+    # In case a placeholder gets wrapped or clipped, this pattern will only match the
+    # head of a placeholder not tails on subsequent lines
+    _placeholder_pattern = re.compile("(\0\1*)")
 
-    # A tail must occur at the beginning of a line but may be preceded by spaces
-    # when `align != "left"`
-    _placeholder_tail = re.compile("^( *)(\1+)")
+    # A tail must occur at the beginning of a line but may be preceded by padding
+    # spaces when `align != "left"` and `wrap != "clip"`
+    _tail_pattern = re.compile("^( *)(\1+)")
 
     def __init__(self, *args, **kwargs):
-        # The order of setting layout, align and wrap is unknown
         self._initialized = False
         super().__init__(*args, **kwargs)
         del self._initialized
@@ -41,7 +40,7 @@ class TextEmbed(urwid.Text):
         text_canv = fix_text_canvas_attr(super().render(size))
         text = text_canv.text
         canvases = []
-        placeholder = __class__._placeholder
+        placeholder_pattern = __class__._placeholder_pattern
         embedded_canvs = self._embedded_canvs
         tail = None
         top = 0
@@ -55,13 +54,13 @@ class TextEmbed(urwid.Text):
         else:
             embedded_canvs_iter = iter(embedded_canvs)
 
-        for row, line in enumerate(text):
+        for row_index, line in enumerate(text):
             line = line.decode()
             if clipped:
                 if line.startswith("\1"):  # align != "left"
-                    canv_index = text_canv_content[row][0][0]
+                    canv_index = text_canv_content[row_index][0][0]
                     canv_start, tail_canv = embedded_canvs[canv_index]
-                    left_trim = -translation[row][0][0]
+                    left_trim = -translation[row_index][0][0]
                     # the placeholder is clipped => left_trim > canv_start
                     tail_width = tail_canv.cols() - (left_trim - canv_start)
                     tail = (tail_width, tail_canv)
@@ -78,10 +77,10 @@ class TextEmbed(urwid.Text):
                 canvases.append((partial_canv, None, focus))
                 n_lines = 0
                 top += 1
-            elif placeholder.search(line):
+            elif placeholder_pattern.search(line):
                 append_text_lines()
                 if clipped:
-                    for attr, *_ in text_canv_content[row]:
+                    for attr, *_ in text_canv_content[row_index]:
                         if isinstance(attr, int):
                             break
                     embedded_canvs_iter = islice(embedded_canvs, attr, None)
@@ -100,10 +99,9 @@ class TextEmbed(urwid.Text):
         return urwid.CanvasCombine(canvases)
 
     def set_layout(self, *args, **kwargs):
-        # The order of setting layout, align and wrap is unknown
-        self._layout_is_set = False
+        self._setting_layout = True
         super().set_layout(*args, **kwargs)
-        del self._layout_is_set
+        del self._setting_layout
         self._update_canv_start_pos()
 
     def set_text(self, markup):
@@ -116,7 +114,7 @@ class TextEmbed(urwid.Text):
         self._update_canv_start_pos()
 
     def _update_canv_start_pos(self):
-        if not (self._initialized and self._layout_is_set and self.wrap == "clip"):
+        if not (self._initialized and not self._setting_layout and self.wrap == "clip"):
             return
         # - Text is clipped per line.
         # - Since the pad/trim amount in the translation (produced by
@@ -124,15 +122,14 @@ class TextEmbed(urwid.Text):
         #   wrt the layout width (maxcol), the position of an embedded widgets on its
         #   respective line should be relative to the start of the line, not considering
         #   alignment.
+        find_placeholders = __class__._placeholder_pattern.finditer
         embedded_canvs_iter = iter(self._embedded_canvs)
         self._embedded_canvs = [
             # Using `Text.pack()` instead of `match.start()` directly to account for
             # wide characters
             (urwid.Text(line[: match.start()]).pack()[0], canv)
             for line in self.text.splitlines()
-            for match, (_, canv) in zip(
-                __class__._placeholder.finditer(line), embedded_canvs_iter
-            )
+            for match, (_, canv) in zip(find_placeholders(line), embedded_canvs_iter)
         ]
 
     @staticmethod
@@ -142,11 +139,9 @@ class TextEmbed(urwid.Text):
             new_markup = []
             for markup in markup:
                 if isinstance(markup, tuple) and isinstance(markup[0], int):
-                    maxcols, widget = markup
-                    new_markup.append(
-                        (len(embedded_canvs), "\0" + "\1" * (maxcols - 1))
-                    )
-                    embedded_canvs.append((None, widget.render((maxcols, 1))))
+                    maxcol, widget = markup
+                    new_markup.append((len(embedded_canvs), "\0" + "\1" * (maxcol - 1)))
+                    embedded_canvs.append((None, widget.render((maxcol, 1))))
                 else:
                     new_markup.append(markup)
             return new_markup, embedded_canvs
@@ -169,7 +164,7 @@ class TextEmbed(urwid.Text):
             #   the tail
             # - Only one possible occurence of a tail per line
             # - Might be preceded by padding spaces when `align != "left"`
-            _, padding, tail_string, line = __class__._placeholder_tail.split(line)
+            _, padding, tail_string, line = __class__._tail_pattern.split(line)
 
             if padding:
                 # Can use `len(padding)` since all characters should be spaces
@@ -177,45 +172,46 @@ class TextEmbed(urwid.Text):
                 canvases.append((canv, None, focus, len(padding)))
                 line_index += len(padding)
 
-            cols, tail_canv = tail
+            tail_width, tail_canv = tail
             canv = urwid.CompositeCanvas(tail_canv)
-            canv.pad_trim_left_right(cols - tail_canv.cols(), len(tail_string) - cols)
+            canv.pad_trim_left_right(
+                tail_width - tail_canv.cols(), len(tail_string) - tail_width
+            )
             canvases.append((canv, None, focus, len(tail_string)))
-            line_index += cols
+            line_index += tail_width
 
             if not line:
                 tail = (
-                    (cols - len(tail_string), tail_canv)
-                    if len(tail_string) < cols
+                    (tail_width - len(tail_string), tail_canv)
+                    if len(tail_string) < tail_width
                     else None
                 )
                 return urwid.CanvasJoin(canvases), tail
             tail = None
 
-        placeholder = __class__._placeholder
+        placeholder_pattern = __class__._placeholder_pattern
         embedded_canvs_iter = iter(embedded_canvs)
 
-        for string in placeholder.split(line):
-            if not string:
+        for part in placeholder_pattern.split(line):
+            if not part:
                 continue
 
-            if placeholder.fullmatch(string):
+            if placeholder_pattern.fullmatch(part):
                 _, canv = next(embedded_canvs_iter)
-                # `len(string)`, in case the placeholder has been wrapped
-                canvases.append((canv, None, focus, len(string)))
-                line_index += len(string)
-                if len(string) != canv.cols():
-                    tail = (canv.cols() - len(string), canv)
+                # `len(part)`, in case the placeholder was wrapped
+                canvases.append((canv, None, focus, len(part)))
+                line_index += len(part)
+                if len(part) != canv.cols():
+                    tail = (canv.cols() - len(part), canv)
             else:
-                w = urwid.Text(string)
-                # Should't use `len(string)` because of wide characters
-                maxcols, _ = w.pack()
+                # Should't use `len(part)` because of wide characters
+                maxcol = urwid.Text(part).pack()[0]
                 canv = urwid.CompositeCanvas(line_canv)
                 canv.pad_trim_left_right(
-                    -line_index, line_index + maxcols - line_canv.cols()
+                    -line_index, line_index + maxcol - line_canv.cols()
                 )
-                canvases.append((canv, None, focus, maxcols))
-                line_index += maxcols
+                canvases.append((canv, None, focus, maxcol))
+                line_index += maxcol
 
         return urwid.CanvasJoin(canvases), tail
 
@@ -224,14 +220,14 @@ def fix_text_canvas_attr(canv: urwid.TextCanvas) -> urwid.TextCanvas:
     """Workaround for a bug in in `urwid.text_layout.StandardTextLayout`.
 
     When `wrap=clip, align=center` and there's a line starting with a markup that has
-    a display attribute, when the render width (maxcols) is one less than the line's
+    a display attribute, when the render width (maxcol) is one less than the line's
     width (in screen columns, not characters), the line is rendered as an empty
     string.
 
     See https://github.com/urwid/urwid/issues/542.
     """
-    for line in canv._attr:
-        if line[0] == (None, 0):
-            del line[0]
+    for line_attr in canv._attr:
+        if line_attr[0] == (None, 0):
+            del line_attr[0]
 
     return canv
