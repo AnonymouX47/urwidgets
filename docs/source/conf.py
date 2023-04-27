c9fe2336ebc3aca87b61e27cf293547be069a8d8
AnonymouX47 <anonymoux47@gmail.com>
2023-04-27 13:49:18 +0100

    docs: Update docs build config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,25 +3,53 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+from urwidgets import __version__
 
+# -- Project information -----------------------------------------------------
 project = 'urWIDgets'
 copyright = '2023, Toluwaleke Ogundipe'
 author = 'Toluwaleke Ogundipe'
+release = __version__
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = []
-
-templates_path = ['_templates']
-exclude_patterns = []
-
-
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.intersphinx",
+    "sphinx_toolbox.github",
+    "sphinx_toolbox.sidebar_links",
+    "sphinx_toolbox.more_autosummary",
+]
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+html_theme = "furo"
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+# -- Options for extensions ----------------------------------------------
+
+# # -- sphinx-autodoc -----------------------------------------------------
+autodoc_default_options = {
+    "members": True,
+    "show-inheritance": True,
+    "member-order": "bysource",
+    "autosummary": True,
+    "autosummary-nosignatures": True,
+}
+autodoc_typehints = "description"
+autodoc_typehints_format = "fully-qualified"
+autodoc_typehints_description_target = "documented"
+autodoc_member_order = "bysource"
+autodoc_inherit_docstrings = False
+
+# # -- sphinx-intersphinx ----------------------------------------------
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+    "urwid": ("https://urwid.org", None),
+}
+
+# # -- sphinx_toolbox-github ----------------------------------------------
+github_username = "AnonymouX47"
+github_repository = "urwidgets"
+
+# # -- sphinx_toolbox-more_autosummary ----------------------------------------------
+autodocsumm_member_order = "bysource"
