f3a7508cef94af1ecc7e65260408b3e0813d30e6
AnonymouX47 <anonymoux47@gmail.com>
2023-05-14 22:30:47 +0100

    chore: Bump package version to `0.1.0`

--- src/urwidgets/__init__.py
+++ src/urwidgets/__init__.py
@@ -34,7 +34,7 @@ from .text_embed import (
     parse_text,
 )
 
-version_info = (0, 1, 0, "dev")
+version_info = (0, 1, 0)
 
 # Follows https://semver.org/spec/v2.0.0.html
 __version__ = ".".join(map(str, version_info[:3]))
